ebdfcdfc5ef80ad763bbeed09e6e52dedb10bcca
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-06-19 19:55:47 +0200

    commiting last state

---
+++ src/routes/basic/calculator.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Navbar from "../../lib/navbar.svelte";
+</script>
+
+<Navbar />
+
+<div class="centered">
+    <table>
+        <tr><td><input /></td></tr>
+    </table>
+</div>
+
+<style>
+    .centered {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+}
+</style>
