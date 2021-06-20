36e030e28cd1432c4e1ca4064d6252fa3d95e235
Mathis <handymathis@gmail.com>
2021-06-20 16:49:33 +0200

    Try to add types to links [#4]

--- src/fix-typos.js
+++ src/fix-typos.js
@@ -5,26 +5,17 @@ try {
 	const data = fs.readFileSync("./build/index.html", "utf8");
 	let newData = data
 		.replace("/favicon.png", "favicon.png")
-		.replace("<head>\n", "<head><base href='./'>\n")
+		.replace("<head>\n", "<head><base href='/'>\n")
 
 	const oldstr = "/./";
 	const newstr = "./";
-	const preloadOld = "rel=\"modulepreload\" ";
-	const preloadNew = "rel=\"modulepreload\"type=\"text/html\"";
-	const stylesheetOld = "rel=\"stylesheet\" ";
-	const stylesheetNew = "rel=\"stylesheet\"type=\"text/css\"";
 
-	while (newData.indexOf(oldstr) > -1) {
+	/*while (newData.indexOf(oldstr) > -1) {
 		newData = newData.replace(oldstr, newstr);
-	}
-	while (newData.indexOf(preloadOld) > -1) {
-		newData = newData.replace(preloadOld, preloadNew);
-	}
-	while (newData.indexOf(stylesheetOld) > -1) {
-		newData = newData.replace(stylesheetOld, stylesheetNew);
-	}
+	}*/
 
 	fs.writeFileSync("./build/index.html", newData);
+	fs.writeFileSync("./build/.nojekyll", "");
 } catch (e) {
 	console.error(e);
 }
