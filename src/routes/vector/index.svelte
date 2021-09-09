5ab828c1c927d89e0eeddfa7bf4a1cc92ec6ec0f
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-09-09 15:12:20 +0200

    [TASK] implemented vector linear checker

--- src/routes/vector/index.svelte
+++ src/routes/vector/index.svelte
@@ -4,7 +4,8 @@
 	import type { ListSelectorElement } from '../../typings/ListSelectorElement';
 
 	let apps: ListSelectorElement[] = [
-		{ name: 'Vector angle calculator', description: 'Calculates the angle between to vectors', route: '/vector/angle-calculator' }
+		{ name: 'Vector angle calculator', description: 'Calculates the angle between to vectors', route: '/vector/angle-calculator' },
+		{ name: 'Vector linear dependency', description: 'checks if two vectors are linear dependent', route: '/vector/linear-checker' }
 	];
 </script>
 
