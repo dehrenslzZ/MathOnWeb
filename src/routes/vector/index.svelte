e6b906f5db2c99689f37432e79cec565794bc7f9
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-09-10 08:12:39 +0200

    [TASK] added method that calculates a line from two vectors

--- src/routes/vector/index.svelte
+++ src/routes/vector/index.svelte
@@ -6,7 +6,8 @@
 	let apps: ListSelectorElement[] = [
 		{ name: 'Vector angle calculator', description: 'Calculates the angle between to vectors', route: '/vector/angle-calculator' },
 		{ name: 'Vector linear dependency', description: 'checks if two vectors are linear dependent', route: '/vector/linear-checker' },
-		{ name: 'Is Vector on line', description: 'Checks if a vector is on a line', route: '/vector/check-vector-on-line' }
+		{ name: 'Is Vector on line', description: 'Checks if a vector is on a line', route: '/vector/check-vector-on-line' },
+		{ name: 'Get vector-line from points', description: 'Generates a line from two points (vectors)', route: '/vector/generate-line' }
 	];
 </script>
 
