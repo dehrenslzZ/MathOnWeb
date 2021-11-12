a98b80d1c5abb5a09b3000dae9bebc56efff2933
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-11-12 09:01:46 +0100

    [TASK] added base for 4-field-table-calculator

---
+++ src/routes/stochastics/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Navbar from '../../lib/navbar.svelte';
+	import ListSelector from '../../lib/listSelector.svelte';
+	import type { ListSelectorElement } from '../../typings/ListSelectorElement';
+
+	let apps: ListSelectorElement[] = [
+		{
+			name: '4 field table',
+			description: 'Can calculate a 4 field table',
+			route: '/stochastics/4-field-table'
+		},
+	];
+</script>
+
+<Navbar />
+
+<ListSelector elements={apps} />
