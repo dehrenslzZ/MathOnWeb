87e2ddd00f833eea03807fc99b21aecf2974b1dc
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-11-17 10:26:56 +0100

    [TASK] added four field table component

---
+++ src/lib/stochastics/four-field-table.component.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    export let AB: number;
+    export let aB: number;
+    export let BA: number;
+    export let ab: number;
+
+    export let B_ALL: number;
+    export let b_ALL: number;
+    export let A_ALL: number;
+    export let a_ALL: number;
+    export let ALL: number;
+</script>
+
+<div class="field-table">
+    <div class="table-element special" />
+    <div class="table-element">A</div>
+    <div class="table-element">a</div>
+    <div class="table-element special" />
+
+    <div class="table-element">B</div>
+    <input class="table-element" bind:value={AB} />
+    <input class="table-element" bind:value={aB} />
+    <input class="table-element" bind:value={B_ALL} />
+
+    <div class="table-element">b</div>
+    <input class="table-element" bind:value={BA} />
+    <input class="table-element" bind:value={ab} />
+    <input class="table-element" bind:value={b_ALL} />
+
+    <div class="table-element special" />
+    <input class="table-element" bind:value={A_ALL} />
+    <input class="table-element" bind:value={a_ALL} />
+    <input class="table-element" bind:value={ALL} />
+
+</div>
+
+<style lang="scss">
+    @import "../../styles/4-field-table.scss";
+</style>
+
--- src/routes/stochastics/4-field-table.svelte
+++ src/routes/stochastics/4-field-table.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import FourFieldTable from '$lib/stochastics/four-field-table.component.svelte';
     import Navbar from '$lib/navbar.svelte';
 </script>
 
@@ -7,7 +8,17 @@
 
 <div class="centered">
     <div class="container">
-        
+        <FourFieldTable 
+            AB={0}
+            aB={0}
+            BA={0}
+            ab={0}
+            B_ALL={0}
+            b_ALL={0}
+            A_ALL={0}
+            a_ALL={0}
+            ALL={0}
+        />
     </div>
 </div>
 
