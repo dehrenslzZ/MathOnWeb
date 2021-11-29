b8f4d100127f0ae495e925d2d42120e305ac5744
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-11-29 09:00:00 +0100

    [TASK] created unsafe version of calculator (Input not working)

--- src/routes/basic/triangle-completer.svelte
+++ src/routes/basic/triangle-completer.svelte
@@ -1,31 +1,49 @@
 <script lang="ts">
 	import { DefaultsProvider } from '$lib/defaults-provider';
 	import Navbar from '$lib/navbar.svelte';
+	import Snackbar from '$lib/snackbar.svelte';
 	import solveTriangle from '$lib/triangle-calculator';
 	let triangle = new DefaultsProvider().getDefaultTriangle();
+	let showSnackbar = false;
+	let errorSnackbar = '';
 
 	function onChange(event: any, field: string) {
 		let copy_triangle = Object.assign({}, triangle);
-		copy_triangle[field] = +event.target.value;
-		triangle = solveTriangle(copy_triangle);
+		copy_triangle[field] = (+event.target.value) * Math.PI / 180;
+		triangle = copy_triangle;
+	}
+
+	function solve() {
+		const [resp_triangle, err] = solveTriangle(triangle);
+		if (err.errorOccurred) {
+			errorSnackbar = err.message;
+			showSnackbar = true;
+			setTimeout(() => {
+				showSnackbar = false;
+				errorSnackbar = '';
+			}, 2000);0
+		} else {
+			triangle = resp_triangle;
+		}
 	}
 </script>
 
 <Navbar />
+{#if showSnackbar}
+	<Snackbar message={errorSnackbar} />
+{/if}
 <div class="center">
 	<div class="triangle-container">
 		<div class="outer-triangle">
 			<input
 				type="number"
 				class="input-kath-left"
-				on:change={(event) => onChange(event, 'kathete1')}
-				value={triangle.kathete1}
+				bind:value={triangle.kathete1}
 			/>
 			<input
 				type="number"
 				class="input-kath-bottom"
-				on:change={(event) => onChange(event, 'kathete2')}
-				value={triangle.kathete2}
+				bind:value={triangle.kathete2}
 			/>
 			<div class="triangle">
 				<div class="bottom-kath" />
@@ -35,24 +53,24 @@
 			<input
 				type="number"
 				class="input-hypothenuse"
-				on:change={(event) => onChange(event, 'hypothenuse')}
-				value={triangle.hypothenuse}
+				bind:value={triangle.hypothenuse}
 			/>
 		</div>
 		<div class="input-container">
 			<div class="input-flex">
-				<input class="triangle-input" on:change={(event) => onChange(event, 'alpha')} />
+				<input class="triangle-input" value="90" disabled />
 				<p>Alpha angle</p>
 			</div>
 			<div class="input-flex">
-				<input class="triangle-input" on:change={(event) => onChange(event, 'beta')} />
+				<input class="triangle-input" on:change={(event) => onChange(event, 'beta')} value={triangle.beta * 180 / Math.PI} />
 				<p>Beta angle</p>
 			</div>
 			<div class="input-flex">
-				<input class="triangle-input" on:change={(event) => onChange(event, 'gamma')} />
+				<input class="triangle-input" on:change={(event) => onChange(event, 'gamma')} value={triangle.gamma * 180 / Math.PI} />
 				<p>Gamma angle</p>
 			</div>
 		</div>
+		<button class="calculate-button" on:click={solve}>Solve</button>
 	</div>
 </div>
 
