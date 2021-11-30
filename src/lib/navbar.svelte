cff60e1327d61404d439760c92f1ab20f7870de9
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-11-30 11:05:46 +0100

    [TASK] first changes on navbar style

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,4 +1,9 @@
-<div class="navbar">
+<script lang="ts">
+	let navbarExtended = false;
+</script>
+
+<div class={`navbar ${navbarExtended ? 'extended' : ''}`}>
+	<button class="navbar-toggler" on:click={() => {navbarExtended = !navbarExtended}}></button>
 	<div class="navbar-menu">
 		<a href="/">Home</a>
 		<a href="/basic">Basic</a>
@@ -7,31 +12,6 @@
 	</div>
 </div>
 
-<style lang="css">
-	.navbar {
-		width: 99vw;
-		height: 70px;
-		background: #2e2d35;
-		border-radius: 10px;
-	}
-	.navbar-menu {
-		width: 100%;
-		height: 100%;
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		gap: 20px;
-	}
-	.navbar-menu a {
-		color: white;
-		font-family: 'Roboto', sans-serif;
-		font-size: 1.4em;
-		cursor: pointer;
-		transition: 0.3s;
-		text-decoration: none;
-	}
-	.navbar-menu a:hover {
-		color: #acacac;
-	}
+<style lang="scss">
+	@import "../styles/navbar.scss";
 </style>
