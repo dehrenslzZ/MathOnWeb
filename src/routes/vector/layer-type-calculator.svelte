99d072eddcf13efdd55ae33cb9bdbb4bb244eec9
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-12-01 10:53:05 +0100

    [TASK] fixed layer type converter style

--- src/routes/vector/layer-type-calculator.svelte
+++ src/routes/vector/layer-type-calculator.svelte
@@ -17,4 +17,11 @@
 
 <style>
 	@import '../../styles/general.scss';
+
+	@media only screen and (max-height: 1100px) and (min-height: 900px) {
+		.container {
+			margin-top: 30%;
+			transform: scale(1.7) !important;
+		}
+	}
 </style>
