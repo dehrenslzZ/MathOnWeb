455a0bc3fb17f3edce19122aeb2afa8a008cef6e
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-12-01 09:41:11 +0100

    [TASK] excluded styles from homepage

--- src/lib/index.component.svelte
+++ src/lib/index.component.svelte
@@ -14,29 +14,5 @@
 </div>
 
 <style lang="css">
-	.centered {
-		position: absolute;
-		top: 20%;
-		left: 50%;
-		transform: translate(-50%, -20%);
-	}
-	.container {
-		width: 60vw;
-		height: 50vh;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		gap: 10px;
-	}
-	.container h1 {
-		color: white;
-		font-family: 'Roboto', sans-serif;
-		font-size: 2em;
-	}
-	.container p {
-		color: white;
-		font-family: 'Roboto', sans-serif;
-		text-overflow: clip;
-		font-size: 1.2em;
-	}
+	@import "../styles/homepage.scss";
 </style>
