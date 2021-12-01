de55c0d6c4ce041611007ff287dc59289a244c77
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-12-01 09:48:37 +0100

    [TASK] improved list selector style

--- src/lib/listSelector.svelte
+++ src/lib/listSelector.svelte
@@ -12,7 +12,7 @@
 	/>
 </svelte:head>
 
-<div class="container">
+<div class="content-container">
 	{#each elements as element, _}
 		<a class="list-element" href={element.route}>
 			<h3>{element.name}</h3>
@@ -22,35 +22,5 @@
 </div>
 
 <style lang="css">
-	.container {
-		width: 100vw;
-		height: 100vh;
-		margin-top: 5em;
-		overflow: hidden scroll;
-		display: flex;
-		align-items: center;
-		flex-direction: column;
-		gap: 10px;
-		-ms-overflow-style: none;
-		scrollbar-width: none;
-	}
-	.container::-webkit-scrollbar {
-		display: none;
-	}
-	.list-element {
-		width: 80%;
-		height: fit-content;
-		display: grid;
-		grid-template-columns: 30% 70%;
-		background: #232228;
-		text-align: center;
-		text-decoration: none;
-		color: white;
-		border-radius: 10px;
-		font-family: 'Roboto', sans-serif;
-		transition: 0.3s;
-	}
-	.list-element:hover {
-		filter: brightness(1.4);
-	}
+	@import "../styles/list-selector.scss";
 </style>
