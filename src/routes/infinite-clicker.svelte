5f2d2a1c6458ec64d67bce24b330d977ccf9c892
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-12-08 12:41:47 +0100

    [TASK] added easter egg ;)

---
+++ src/routes/infinite-clicker.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let x = window.innerWidth / 2;
+    let y = window.innerHeight / 2;
+    
+    function onClick() {
+        x = getRandom(50, window.innerWidth - 100);
+        y = getRandom(100, window.innerHeight - 100);
+    }
+
+    function getRandom(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+</script>
+
+<div class="infinite-clicker" style={`top: ${y}px; left: ${x}px;`} on:click={onClick} />
+
+<style lang="scss">
+    @import '../styles/infinite-clicker.scss';
+</style>
