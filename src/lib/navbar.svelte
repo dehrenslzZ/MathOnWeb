c9defe75dfa710a51f7b00c20a2a5467f1bd14bf
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-12-08 12:26:59 +0100

    [TASK] navbar extending if clicking outside

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -4,9 +4,24 @@
 	import { faAlignJustify } from '@fortawesome/free-solid-svg-icons';
 	import {_} from 'svelte-i18n';
 	let navbarExtended = false;
+
+	/**
+	 * Checks if the user clicked outside of the navbar and the 
+	 * navbar gets minimized, if it was extended before.
+	 * 
+	 * @param event The PointerEvent triggered by a click
+	 */
+	const handleClick = (event: PointerEvent) => {
+		if (!document.getElementById('navbar').contains(event.target as Node)) {
+			navbarExtended = false;
+		}		
+	};
+
+	// Adding click listener to the whole window
+	window.addEventListener('click', handleClick);
 </script>
 
-<div class={`navbar ${navbarExtended ? 'extended' : ''}`}>
+<div id="navbar" class={`navbar ${navbarExtended ? 'extended' : ''}`}>
 	<div
 		class="navbar-toggler"
 		on:click={() => {
