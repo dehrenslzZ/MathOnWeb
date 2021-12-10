30de1e615c4070773d70f7aaf825d394c980aa0d
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-12-10 08:50:35 +0100

    [TASK] implemented not found page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,14 @@
+<div class="centered">
+    <div class="container">
+        <div class="error-heading">404 Not Found</div>
+        <div class="error-container">
+            Not found
+        </div>
+    </div>
+</div>
+
+
+<style lang="scss">
+    @import "../styles/general.scss";
+    @import "../styles/error.scss";
+</style>
