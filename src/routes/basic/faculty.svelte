a5864a517a9290980a032347c0f67d63188711ea
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-12-10 08:24:06 +0100

    [TASK] implemented function for calculating the facultry

---
+++ src/routes/basic/faculty.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { faculty } from "$lib/custom-math";
+
+
+    let input = 0;
+    
+</script>
+
+<div class="centered">
+    <div class="container">
+        <input class="larger-input" type="number" bind:value={input} />
+        <div class="result-form">
+            {faculty(input)}
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+    @import "../../styles/general.scss";
+</style>
