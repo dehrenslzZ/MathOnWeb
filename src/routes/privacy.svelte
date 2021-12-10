c0f264282ddb931f9233683d578106418c19ac11
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-12-10 09:15:38 +0100

    [TASK] added privacy page without text

---
+++ src/routes/privacy.svelte
@@ -0,0 +1,12 @@
+<div class="centered">
+    <div class="container">
+        <h1 class="privacy-heading">Privacy</h1>
+        <div class="privacy-text">Some privacy placeholder text</div>
+    </div>
+</div>
+
+
+<style lang="scss">
+    @import "../styles/general.scss";
+    @import "../styles/privacy.scss";
+</style>
