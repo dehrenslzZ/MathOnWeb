e4b611462d765e20178f5ffe362ed1c8b28bcb7e
Mathis Burger <57145815+MathisBurger@users.noreply.github.com>
2022-03-28 09:22:09 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -17,9 +17,8 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		}),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		})
+	
 	}
 };
 
