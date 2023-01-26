2e2987195bb2b5831a0566225222d5f500ebde3d
MathisBurger <MathisBurger@users.noreply.github.com>
2023-01-26 15:23:27 +0000

    Deploying to gh-pages from @ MathisBurger/MathOnWeb@e83b449f2496029cd54bdb16db79c764280a0501 🚀

--- _app/chunks/ncr-calculator-b8391fd4.js
+++ _app/chunks/ncr-calculator-b8391fd4.js
@@ -1,1 +1,1 @@
-import{f as o}from"./custom-math-d14d206d.js";function m(t){const a=o(t.n),s=o(t.k)*o(t.n-t.k);return a/s}export{m as c};
+import{f as o}from"./custom-math-d14d206d.js";function n(t){const a=o(t.n),c=o(t.k)*o(t.n-t.k);return a/c}export{n as c};
