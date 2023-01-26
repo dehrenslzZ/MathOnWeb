064a497e6981f654246b25034be367426f273885
MathisBurger <MathisBurger@users.noreply.github.com>
2023-01-26 20:42:05 +0000

    Deploying to gh-pages from @ MathisBurger/MathOnWeb@199d0d99248baccc4f9b853dc9fc23445a167008 🚀

---
+++ _app/chunks/theme-68f09822.js
@@ -0,0 +1,1 @@
+import{w as o}from"./index-c120acd1.js";var e;const r=o((e=localStorage.getItem("color-theme"))!=null?e:"dark");r.subscribe(t=>{localStorage.setItem("color-theme",t)});export{r as t};
